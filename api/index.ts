70b769bfb7888c57b170c0bf5fd9fb5b1784db9e
Steve Kinney <hello@stevekinney.net>
2025-05-24 11:59:25 -0600

    Allow CORS in development

--- api/index.ts
+++ api/index.ts
@@ -3,15 +3,12 @@ import { cors } from 'hono/cors';
 import { api } from './routes';
 
 const app = new Hono();
+const port = process.env['PORT'] || 3000;
 
-// Enable CORS for frontend
-app.use(
-  '*',
-  cors({
-    origin: 'http://localhost:5173', // Vite's default port
-    credentials: true,
-  }),
-);
+if (import.meta.env.MODE === 'development') {
+  // Enable CORS in development mode
+  app.use('*', cors({ origin: '*' }));
+}
 
 // API routes
 app.route('/api', api);
@@ -20,8 +17,6 @@ app.route('/api', api);
 app.get('/', (c) => {
   return c.json({ message: 'Bun + Vite Full-Stack Server Running!' });
 });
-
-const port = process.env['PORT'] || 3000;
 
 console.log(`🚀 Server running on http://localhost:${port}`);
 
