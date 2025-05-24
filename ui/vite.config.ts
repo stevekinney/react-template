9fe167facdf0ba37c0c4d54824ad8e77cd45218c
Steve Kinney <hello@stevekinney.net>
2025-05-24 11:59:11 -0600

    Use the server port environment variable

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,12 +2,14 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const port = process.env['PORT'] || 3000;
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: `http://localhost:${port}`,
         changeOrigin: true,
       },
     },
